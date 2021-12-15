85cdb70f5324507e7a049a8e0d2caaf9b7a026aa
ishanishah21 <ishanishah2110@gmail.com>
2021-12-15 21:53:41 +0530

    - init commit

---
+++ app/src/main/java/com/ishani/roomdemo/db/SubscriberDAO.kt
@@ -0,0 +1,23 @@
+package com.ishani.roomdemo.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SubscriberDAO {
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_table")
+    fun getAllSubscribersData(): Flow<List<Subscriber>>
+}
